905f3d1a1347cf1f9735ca4aef7cd47612bb7906
Anton <anton.abyzov@gmail.com>
2017-07-24 02:26:58 +0300

    Running time complexity (prime numbers)

---
+++ RunningTimeAndComplexity/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RunningTimeAndComplexity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            while (n-- > 0)
+            {
+                var number = Convert.ToInt32(Console.ReadLine());
+                DetermineIfPrime(number);
+            }
+        }
+
+        private static void DetermineIfPrime(int number)
+        {
+            if (number < 2)
+            {
+                Console.WriteLine("Not prime");
+                return;
+            }
+            for (var i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine("Not prime");
+                    return;
+                }
+            }
+            Console.WriteLine("Prime");
+        }
+    }
+}
