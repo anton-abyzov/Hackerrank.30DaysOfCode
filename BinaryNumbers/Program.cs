beac758351c61333f414211a18d5e9e90af9026a
Anton <anton.abyzov@gmail.com>
2017-07-23 20:38:36 +0300

    initial, several tasks solved

---
+++ BinaryNumbers/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace BinaryNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Int32.MaxValue);
+            int n = Convert.ToInt32(Console.ReadLine());
+            //string binaryN = Convert.ToString(n, 2);
+            string binaryN = Convert10To2Radix(n);
+            int streak = FindLongestStreak(binaryN);
+            Console.WriteLine(streak);
+        }
+
+        private static int FindLongestStreak(string binaryN)
+        {
+            if (string.IsNullOrEmpty(binaryN))
+                return 0;
+            var charArray = binaryN.ToCharArray();
+            var longestStreak = 0;
+            var currentStreak = 0;
+            for (var i = 0; i < charArray.Length; i++)
+            {
+                var digit = int.Parse(charArray[i].ToString());
+                if (digit == 1)
+                {
+                    currentStreak++;
+                }
+                if (digit == 0)
+                {
+                    if (currentStreak > longestStreak)
+                        longestStreak = currentStreak;
+                    currentStreak = 0;
+                }
+            }
+            return Math.Max(longestStreak, currentStreak);
+        }
+
+        private static string Convert10To2Radix(int value)
+        {
+            var stack = new Stack<int>();
+            while (value > 0)
+            {
+                stack.Push(value % 2);
+                value = value / 2;
+            }
+            //var result = stack.ToArray().Select((elem, index) => elem * Math.Pow(10, stack.Count - 1 - index));
+            //return result.Sum().ToString(CultureInfo.InvariantCulture);
+            return String.Join(String.Empty, stack.ToArray());
+        }
+    }
+}
--- BitwiseAnd/Program.cs
+++ BitwiseAnd/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BitwiseAnd
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                string[] tokens_n = Console.ReadLine().Split(' ');
+                int n = Convert.ToInt32(tokens_n[0]);
+                int k = Convert.ToInt32(tokens_n[1]);
+
+                var resultSet = new List<int>();
+                for (var i = 1; i <= n; i++)
+                    for (var j = i + 1; j <= n; j++)
+                    {
+                        var bitwiseAnd = i & j;
+                        if (bitwiseAnd < k)
+                            resultSet.Add(bitwiseAnd);
+                    }
+
+                var maxValue = resultSet.Max();
+                Console.WriteLine(maxValue);
+            }
         }
     }
 }
